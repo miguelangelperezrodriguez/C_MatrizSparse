c6e5c338e05012022859d4e0c7af7e4a18158c6d
miguelangelperezrodriguez <45273469+miguelangelperezrodriguez@users.noreply.github.com>
2025-01-19 18:16:15 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -29,20 +29,20 @@ float datos_sparse [FILCOLS*FILCOLS] = {0.0F};
 int cargar_datos_sparse (char *nombre_fichero, float datos[]);
 
 // USO DE LA MATRIZ
-float dato_accedido (long f,long c);
+float dato_accedido (long f,long c,float sparse[]);
 // Devuelve anterior valor.
-float almacenar_dato (long f,long c,float valor);
+float almacenar_dato (long f,long c,float valor,float sparse[]);
 
 // ACCESOS RAPIDOS.
 // CARGA EL acc CON LA FILA f Y LA COLUMNA c
 void limpiar_acceso (struct acceso_rapido *acceso);
-void cargar_acceso_rapido (struct acceso_rapido *acc,long f,long c);
-void cargar_acceso_rapido_fila (struct acceso_rapido *acc,long f);
+void cargar_acceso_rapido (struct acceso_rapido *acc,long f,long c,float sparse[]);
+void cargar_acceso_rapido_fila (struct acceso_rapido *acc,long f,float sparse[]);
 
 // ACCESOS RAPIDOS GUARDADOS.
-void limpiar_acceso_mem (struct acceso_rapido *acceso);
-void cargar_acceso_rapido_mem (struct acceso_rapido *acc,long f,long c);
-void cargar_acceso_rapido_fila (struct acceso_rapido *acc,long f);
+//void limpiar_acceso_mem (struct acceso_rapido *acceso);
+//void cargar_acceso_rapido_mem (struct acceso_rapido *acc,long f,long c);
+//void cargar_acceso_rapido_fila (struct acceso_rapido *acc,long f);
 
 struct acceso_rapido acceso_1;
 
@@ -59,16 +59,15 @@ int main(int argc,char *argv[])
 
     // CODIGO SIN FICHEROS
 
-    almacenar_dato(4,2,10.0);
-    printf ("%f\n",dato_accedido(4,2));
-    anterior = almacenar_dato(4,2,40.0);
-    printf ("Anterior : %f . Actual : %f\n",anterior,dato_accedido(4,2));
+    almacenar_dato(4,2,10.0,datos_sparse);
+    printf ("%f\n",dato_accedido(4,2,datos_sparse));
+    anterior = almacenar_dato(4,2,40.0,datos_sparse);
+    printf ("Anterior : %f . Actual : %f\n",anterior,dato_accedido(4,2,datos_sparse));
     printf ("///////////////////////////////////////////\n");
 
     // Uso de taccessos
     limpiar_acceso(&acceder1);
-    cargar_acceso_rapido_fila(&acceder1,4);
-    printf ("%ld : %ld\n",acceder1.nro_fila,acceder1.nro_col);
+    cargar_acceso_rapido_fila(&acceder1,4,datos_sparse);
     for (int i=0;i<FILCOLS;i++)
     {
         printf ("DATO FILA %d ? : %f\n",i,acceder1.datos_fila[i]);
@@ -82,7 +81,7 @@ int main(int argc,char *argv[])
         printf ("%s cargado.\n",n_fichero);
         cargar_datos_sparse(n_fichero, datos_sparse);
         // Ver una lectura
-        printf ("ACCESO : 4.5 = %f\n",dato_accedido(4,5));
+        printf ("ACCESO : 2.2 = %f\n",dato_accedido(2,2,datos_sparse));
     }
 }
 
@@ -105,7 +104,7 @@ int cargar_datos_sparse (char *nombre_fichero, float datos[])
     {
         if ((ptr_tok = strtok (buffer,"; \n")) != NULL)
         {
-            almacenar_dato(f_aux,c_aux,atof(ptr_tok));
+            almacenar_dato(f_aux,c_aux,atof(ptr_tok),datos);
             c_aux++;
             nro_datos++;
             while (ptr_tok != NULL)
@@ -113,7 +112,7 @@ int cargar_datos_sparse (char *nombre_fichero, float datos[])
                 ptr_tok = strtok (NULL, "; \n");
                 if (ptr_tok != NULL)
                 {
-                    almacenar_dato(f_aux,c_aux,atof(ptr_tok));
+                    almacenar_dato(f_aux,c_aux,atof(ptr_tok),datos);
                     c_aux++;
                     nro_datos++;
                 }
@@ -127,21 +126,21 @@ int cargar_datos_sparse (char *nombre_fichero, float datos[])
 }
 
 // FUNCIONES SIMPLES PARA ACCEDER Y MODIFICAR LOS DATOS.
-float dato_accedido (long f,long c)
+float dato_accedido (long f,long c,float sparse[])
 {
-   return datos_sparse [(f-1)*FILCOLS+(c-1)];
+   return sparse [(f-1)*FILCOLS+(c-1)];
 }
 
-float almacenar_dato (long f,long c,float valor)
+float almacenar_dato (long f,long c,float valor,float sparse[])
 {
     float anterior;
-    anterior = datos_sparse[(f-1)*FILCOLS+(c-1)];
-    datos_sparse[(f-1)*FILCOLS+(c-1)] = valor;
+    anterior = sparse[(f-1)*FILCOLS+(c-1)];
+    sparse[(f-1)*FILCOLS+(c-1)] = valor;
     return anterior;
 }
 
 // CARGA EL acc CON LA FILA f Y LA COLUMNA c
-void cargar_acceso_rapido (struct acceso_rapido *acc,long f,long c)
+void cargar_acceso_rapido (struct acceso_rapido *acc,long f,long c,float sparse[])
 {
     float aux;
     long idx;
@@ -152,18 +151,18 @@ void cargar_acceso_rapido (struct acceso_rapido *acc,long f,long c)
     idx = (f-1) * FILCOLS;
     for (int i=0;i<FILCOLS;i++)
     {
-        acc->datos_fila[i]=datos_sparse[idx+i];
+        acc->datos_fila[i]=sparse[idx+i];
     }
 
     for (int i=0;i<FILCOLS;i++)
     {
-        aux = dato_accedido(i,c);
+        aux = dato_accedido(i,c,sparse);
         acc->datos_col[i]=aux;
     }
 
 }
 
-void cargar_acceso_rapido_fila (struct acceso_rapido *acc,long f)
+void cargar_acceso_rapido_fila (struct acceso_rapido *acc,long f,float sparse[])
 {
     long idx;
     acc->nro_fila = f;
@@ -172,7 +171,7 @@ void cargar_acceso_rapido_fila (struct acceso_rapido *acc,long f)
     idx = (f-1) * FILCOLS;
     for (int i=0;i<FILCOLS;i++)
     {
-        acc->datos_fila[i]=datos_sparse[idx+i];
+        acc->datos_fila[i]=sparse[idx+i];
     }
 }
 
